6fb13a9f922af6bdb26705979fb9dc761f599e95
Keith Chason <kchason@users.noreply.github.com>
2023-03-16 12:54:41 +0000

    Allow fast fail in action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Build the arguments for the validator
+args="--built-version ${CASE_VERSION}"
+if [ ${FAST_FAIL} ]; then
+    args="${args} --abort"
+fi
+
 # Determine if the provided path is a directory. If so, then there is filtering and other handling
 # to address. If it is a file, then it is assumed it should be validated.
 if [[ -d "${CASE_PATH}" ]]; then
@@ -11,20 +17,20 @@ if [[ -d "${CASE_PATH}" ]]; then
         for entry in "${CASE_PATH}"/*
         do
             echo "Validating file ${entry} found within ${CASE_PATH}"
-            case_validate "${entry}" --built-version "${CASE_VERSION}"
+            case_validate "${entry} ${args}"
         done
     else 
         # Filter was specified, only run against provided files
         for entry in "${CASE_PATH}"/*."${EXTENSION_FILTER}"
         do
             echo "Validating file ${entry} found within ${CASE_PATH}"
-            case_validate "${entry}" --built-version "${CASE_VERSION}"
+            case_validate "${entry} ${args}"
         done
     fi
 
 elif [[ -f ${CASE_PATH} ]]; then
     echo "Validating file at ${CASE_PATH}"
-    case_validate "${CASE_PATH}" --built-version "${CASE_VERSION}"
+    case_validate "${CASE_PATH} ${args}"
 else
     echo "${CASE_PATH} is not a valid path and the validation cannot continue"
     exit 1
