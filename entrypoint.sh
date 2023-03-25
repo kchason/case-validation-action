4dca8eb098db9375348572bc72da7433bdad5378
Keith Chason <kchason@users.noreply.github.com>
2023-03-25 21:15:37 +0000

    Switch away from eval and wrap filename

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,20 +17,20 @@ if [[ -d "${CASE_PATH}" ]]; then
         for entry in "${CASE_PATH}"/*
         do
             echo "Validating file ${entry} found within ${CASE_PATH}"
-            eval "${cmd} ${entry}"
+            $cmd "${entry}"
         done
     else 
         # Filter was specified, only run against provided files
         for entry in "${CASE_PATH}"/*."${EXTENSION_FILTER}"
         do
             echo "Validating filtered file ${entry} found within ${CASE_PATH}"
-            eval "${cmd} ${entry}"
+            $cmd "${entry}"
         done
     fi
 
 elif [[ -f ${CASE_PATH} ]]; then
     echo "Validating file at ${CASE_PATH}"
-    eval "${cmd} ${CASE_PATH}"
+    $cmd "${CASE_PATH}"
 else
     echo "${CASE_PATH} is not a valid path and the validation cannot continue"
     exit 1
